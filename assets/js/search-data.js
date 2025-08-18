34f5846d1c625479435257de8bccca5d9333e1bc
itsAshna <theashna@gmail.com>
2025-08-18 01:07:41 +0000

    Deploying to gh-pages from @ itsAshna/Venus@5e7ccdb9cb100e3e4e4bd956b202db102f3e3a61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
